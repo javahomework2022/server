bee896d2faa91d2d476164deb2ab38a0d1e7174b
Freeman <freemanwink@outlook.com>
2022-12-11 12:55:53 +0800

    添加Message相关内容

---
+++ src/main/java/com/workonline/server/MessageReceiver.java
@@ -0,0 +1,34 @@
+package com.workonline.server;
+
+
+import com.workonline.util.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class MessageReceiver implements Runnable{
+     Socket socket;
+     ObjectInputStream objectInputStream;
+     public MessageReceiver(Socket socket) throws IOException {
+          this.socket = socket;
+          this.objectInputStream = new ObjectInputStream(socket.getInputStream());
+     }
+     /**
+     * 这个方法会在新的线程里处理接收到的Message
+     */
+     @Override
+     public void run() {
+          while (true){
+               try {
+                    Message message = (Message) objectInputStream.readObject();
+                    /*
+                    * 这儿接收所有用户的message，需要在登录成功和注册成功后将Program.streams添加对应的人和输出流
+                    * TODO
+                     */
+               } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+               }
+          }
+     }
+}
---
+++ src/main/java/com/workonline/server/Program.java
@@ -0,0 +1,43 @@
+package com.workonline.server;
+
+import com.workonline.util.Message;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+
+/**
+ *
+ */
+public class Program {
+    //一个用户名对于一个输出流,登录后添加
+    public static HashMap<String, ObjectOutputStream > streams = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        //主线程接收tcp请求，对于每个tcp新建线程接收Message，在每一个线程里直接调用SendMessage就好了
+
+            try {
+                var serverSocket = new ServerSocket(10099);
+                while (true)
+                {
+                    Socket socket = serverSocket.accept();
+                    new Thread(new MessageReceiver(socket)).start();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+    }
+
+    public static final Object locker = new Object();
+
+    public static void SendMessage(Message message, String username) throws IOException {
+        synchronized (locker) {
+            ObjectOutputStream objectOutputStream = Program.streams.get(username);
+            objectOutputStream.writeObject(message);
+            objectOutputStream.flush();
+        }
+    }
+}
